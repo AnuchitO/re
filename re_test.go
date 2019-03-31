89a519fe9c0e968dcbab37402cfaffc27102e2f8
AnuchitO <anuchitp@thoughtworks.com>
2019-03-31 21:07:09 +0700

    [temp] test example

--- re_test.go
+++ re_test.go
@@ -3,7 +3,13 @@ package main
 import (
 	"errors"
 	"reflect"
+	"sync"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/AnuchitO/re/runner"
 )
 
 func TestSplitArguments(t *testing.T) {
@@ -58,5 +64,24 @@ func TestSplitArguments(t *testing.T) {
 			t.Errorf("expect params is %q but got %q", eParams, params)
 		}
 
+	})
+}
+
+func TestRerun(t *testing.T) {
+	t.Run("example testing", func(t *testing.T) {
+		task := runner.New("go", []string{"version"}...)
+		stop := make(chan struct{})
+		var wg sync.WaitGroup
+		wg.Add(1)
+
+		time.AfterFunc(2*time.Second, func() {
+			close(stop)
+		})
+
+		run(".", task, stop, &wg)
+
+		wg.Wait()
+
+		assert.True(t, true, "should be pass")
 	})
 }
